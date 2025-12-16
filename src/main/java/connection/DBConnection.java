371b9c099ba263019f86d770e0c21808a341bc71
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2025-12-16 17:39:13 +0300

    feat: store connection variables in a .env file using dotenv

--- src/main/java/connection/DBConnection.java
+++ src/main/java/connection/DBConnection.java
@@ -1,13 +1,17 @@
 package connection;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    String jdbcUrl = System.getenv("jdbcUrl");
-    String username = System.getenv("username");
-    String password = System.getenv("password");
+    private final Dotenv dotenv = Dotenv.configure().load();
+
+    String jdbcUrl = dotenv.get("jdbcUrl");
+    String username = dotenv.get("username");
+    String password = dotenv.get("password");
 
     public Connection getDBConnection(){
         try {
