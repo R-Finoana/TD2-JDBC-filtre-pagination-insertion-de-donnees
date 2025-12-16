ec19999469a7e09362d65eab6667276695ed77b9
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2025-12-16 17:57:05 +0300

    chore: make the connexion variables final

--- src/main/java/connection/DBConnection.java
+++ src/main/java/connection/DBConnection.java
@@ -9,12 +9,13 @@ import java.sql.SQLException;
 public class DBConnection {
     private final Dotenv dotenv = Dotenv.configure().load();
 
-    String jdbcUrl = dotenv.get("jdbcUrl");
-    String username = dotenv.get("username");
-    String password = dotenv.get("password");
+    private final String jdbcUrl = dotenv.get("jdbcUrl");
+    private final String username = dotenv.get("username");
+    private final String password = dotenv.get("password");
 
     public Connection getDBConnection(){
         try {
+            assert jdbcUrl != null;
             return DriverManager.getConnection(jdbcUrl, username, password);
         } catch (SQLException e){
             throw new RuntimeException (e);
